f2e271227a354bdea934d8d2e822cf67214555f3
Sheshu <reddysheshasena@gmail.com>
2020-11-30 23:45:26 +0530

    updated script

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ let val2="/images/dice" + random1 +".png";
 
 document.querySelector(".img1").setAttribute("src",val);
 document.querySelector(".img2").setAttribute("src",val2);
+
+if(random>random1)
+{
+    document.querySelector(".win").innerHTML="Player 1 wins !";
+}
+else if(random<random1)
+{
+    document.querySelector(".win").innerHTML="Player 2 wins !";
+}
+else
+{
+    document.querySelector(".win").innerHTML="DRAW !";
+}
